2d19f3ef594615d3806d61a111fab5556efe43a3
James Youn <jamesyoun710@gmail.com>
2015-10-15 16:42:58 -0700

    Annotate

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  location    :string
+#  start       :datetime
+#  end         :datetime
+#  employer_id :integer
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Job < ActiveRecord::Base
   belongs_to :employer
 end
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  location    :string
+#  start       :datetime
+#  end         :datetime
+#  employer_id :integer
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase

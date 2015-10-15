db05ea4d028ae6d94bea8dc99b49d3daa2afc6c1
James Youn <jamesyoun710@gmail.com>
2015-10-15 16:35:47 -0700

    Route for visitors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   devise_for :employers
   resources :jobs
 
   # logged-in root
   authenticated :employer do
     root to: "jobs#index", as: :authenticated_root, via: :get
+  end
+
+  # visotrs root
+  unauthenticated do
+    root to: "static_pages#home"
   end
 end

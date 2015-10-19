fc372e7c0add21d71c57cc3c04531c2d0edb220d
James Youn <jamesyoun710@gmail.com>
2015-10-18 18:42:10 -0700

    Generate devise admin & views

---
+++ app/controllers/admins/regisrations_controller.rb
@@ -0,0 +1,13 @@
+class Admins::RegistrationsController < Devise::RegistrationsController
+  # disable default no_authentication action
+  skip_before_action :require_no_authentication, only: [:new, :create, :cancel]
+  # now we need admin to register new admin
+  prepend_before_action :authentication_scope!, only: [:new, :create, :cancel]
+
+  protected
+
+  def sign(resource_name, resource)
+    # just overwrite the default one to prevent auto sign in as the new sign up
+    # note:  is this a blank overwrite?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
+  devise_for :admins, controllers: { registraions: "admins/registrations" }
   devise_for :employers
   resources :jobs
 
   # logged-in root
+  authenticated :admin do
+    root to: "jobs#index", as: :authenticated_admin_root, via: :get
+  end
+
+  # logged-in root
   authenticated :employer do
-    root to: "jobs#index", as: :authenticated_root, via: :get
+    root to: "jobs#index", as: :authenticated_employer_root, via: :get
   end
 
   # visotrs root

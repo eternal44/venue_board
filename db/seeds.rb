fa69ef4cbe704701dd5c4a6d30aaba132643e6ba
James Youn <jamesyoun710@gmail.com>
2015-10-22 19:47:16 -0700

    Seed db for easier visualization
    Easier visualization to improve UX design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "jamesyoun710@gmail.com",
+            role: "Employer",
+            password: "gooneen44",
+            password_confirmation: "gooneen44")
+
+2.times do |i|
+  email = "example-#{i+1}@example.com"
+  password = "password"
+  User.create( email: email,
+              role: "Employer",
+              password: password,
+              password_confirmation: password)
+end
+
+5.times do |n|
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  User.create(email: email,
+              role: "Worker",
+              password: password,
+              password_confirmation: password)
+end
+
+Profile.all.each do |profile|
+  name = Faker::Name.name
+  profile.update(name: name)
+end
+
+users = User.where('id < 4')
+10.times do
+  title = Faker::Book.title
+  location = Faker::Address.street_address
+  start_time = Faker::Time.between(DateTime.now + 20, DateTime.now)
+  end_time = start_time + 1
+  users.each { |user| user.jobs.create!(title: title,
+                                        location: location,
+                                        start_time: start_time,
+                                        end_time: end_time,
+                                        status: 'Pending') }
+end

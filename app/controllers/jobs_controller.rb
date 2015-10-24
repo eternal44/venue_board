36fa1278e108e0fb172dad650f94814c49c4b5b6
James Youn <jamesyoun710@gmail.com>
2015-10-23 20:03:00 -0700

    Small variable name changes and navbar edits

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,7 @@ class JobsController < ApplicationController
 
   def index
     @q = Job.ransack(params[:q])
-    @job = @q.result(distinct: true)
+    @jobs = @q.result(distinct: true)
   end
 
   def show

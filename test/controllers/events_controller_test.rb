f1d65ff4fef52b85229d862f74cb7671ed7473f9
James Youn <jamesyoun710@gmail.com>
2015-10-28 13:21:13 -0700

    Rebuild event#show action
    All related files rebuilt as well.

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -3,15 +3,15 @@ require 'test_helper'
 class EventsControllerTest < ActionController::TestCase
   def setup
     @valid_event = events(:one)
+    sign_in users(:two)
   end
 
-  # test 'should show event' do
-  #   get :show, id: @valid_event
-  #   assert_equal("MyString", @valid_event.title)
-  # end
+  test 'should show event' do
+    get :show, id: @valid_event
+    assert_equal("MyString", @valid_event.title)
+  end
 
   test 'should get index' do
-    sign_in users(:two)
     get :index
     assert_response :success
     assert_not_nil assigns(@events)
